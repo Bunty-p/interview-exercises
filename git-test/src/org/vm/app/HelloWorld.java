8919b02cede4b134718f4c9852b7a58f4d696ddc
ssykes <ssykes@volunteermatch.org>
2012-11-09 11:57:33 -0800

    Initial code - Git Tests

---
+++ git-test/src/org/vm/app/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.vm.app;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ssykes
+ * Date: 11/9/12
+ * Time: 11:54 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+
+  public static void main(String[] args){
+    System.out.println("hello world");
+  }
+}
