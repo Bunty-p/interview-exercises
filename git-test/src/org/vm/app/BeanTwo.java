70ebbc31bfad1df968086c05220a250d8236e16f
ssykes <ssykes@volunteermatch.org>
2012-11-12 09:55:59 -0800

    commit and push BeanTwo to new-branch-1.

---
+++ git-test/src/org/vm/app/BeanTwo.java
@@ -0,0 +1,20 @@
+package org.vm.app;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ssykes
+ * Date: 11/12/12
+ * Time: 9:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BeanTwo {
+  private int id;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+}
