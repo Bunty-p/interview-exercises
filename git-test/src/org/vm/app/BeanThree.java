87b679f602deae7bcc0d38226fffbad411cd5c52
ssykes <ssykes@volunteermatch.org>
2012-11-12 09:58:25 -0800

    commit and push BeanThree to new-branch-1.

---
+++ git-test/src/org/vm/app/BeanThree.java
@@ -0,0 +1,29 @@
+package org.vm.app;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ssykes
+ * Date: 11/12/12
+ * Time: 9:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BeanThree {
+  private int id;
+  private String name;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
